14675c44d1195233b9ea0dcfe78b0ab3282b39af
SamueleLopresti <eleumas00@gmail.com>
2023-02-09 17:42:47 +0100

    LanciaDadi

---
+++ Prj01_lanciaDadi/src/view/Gioco1.java
@@ -0,0 +1,39 @@
+package view;
+
+import model.Dado;
+
+public class Gioco1 {
+
+	public static void main(String[] args) {
+		
+		Dado d1 = new Dado();
+		Dado d2 = new Dado();
+		
+		final int NUM_LANCI = 100_000;
+		int vittorie = 0;
+		
+		long start = System.currentTimeMillis();
+		for (int i = 0;i<NUM_LANCI;i++) {
+			int r1 = d1.lancia();
+		    System.out.println("Dado 1: " + r1);
+		    
+		    int r2 = d2.lancia();
+		    System.out.println("Dado 2: " + r2);
+		    
+		    if (r1 == r2) {
+		    	vittorie++;
+		    	System.out.println("Hai vinto!");
+		    	
+		    }
+		}
+		
+		long stop = System.currentTimeMillis();
+		
+		System.out.println("Hai vinto: " + vittorie + " volte");
+		
+		System.out.println("La percentuale " + ((double)vittorie/NUM_LANCI));
+		
+		System.out.println("L'elaborazione è durata: " + (stop-start) + "ms");
+		
+	}
+}
